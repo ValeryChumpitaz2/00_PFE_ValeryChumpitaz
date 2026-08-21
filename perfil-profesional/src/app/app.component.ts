6966b92933f0f27d351288335f10123531907177
ValeryChumpitaz2 <valerychumpitaz2@gmail.com>
2026-08-21 08:52:50 -0500

    Actualizado

--- perfil-profesional/src/app/app.component.ts
+++ perfil-profesional/src/app/app.component.ts
@@ -8,5 +8,24 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'perfil-profesional';
+
+  nombre = 'Valery Chumpitaz';
+  profesion= 'Analista de Sistemas';
+  descripcion='Soy estudiante de Análisis de Sistemas interesada en el desarrollo de aplicaciones web y en la creación de soluciones tecnológicas.';
+  
+  correo = 'valery.chumpitaz@vallegrande.edu.pe';
+  ubicacion = 'Lima, Perú';
+  habilidades= ['Angular', 'Java', 'SQL', 'Git', 'HTML', 'CSS']
+
+  proyectos= [
+    {
+      nombre: 'Sistema de Ventas',
+      descripcion: 'Desarrolle un Sistema de Ventas utilizando Angular y Java'
+    },
+    {
+      nombre: 'Aplicacion de Gestion de Tareas',
+      descripcion: 'Desarrole una aplicacion de gestion de tareas utilizando Angular y Java'
+    }
+  ];
+
 }
