941e6d7b3b2ca5dc3b0476996f3a77ddd5964f4c
ValeryChumpitaz2 <valerychumpitaz2@gmail.com>
2026-08-25 11:55:11 -0500

    Lista TS

--- S03-sistema-productos/src/app/components/producto-lista/producto-lista.component.ts
+++ S03-sistema-productos/src/app/components/producto-lista/producto-lista.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, INJECTOR, Input, Output } from '@angular/core';
+import { Producto } from '../../app.component';
+import { ProductoCardComponent } from '../producto-card/producto-card.component';
 
 @Component({
   selector: 'app-producto-lista',
-  imports: [],
+  imports: [ProductoCardComponent],
   templateUrl: './producto-lista.component.html',
   styleUrl: './producto-lista.component.css'
 })
 export class ProductoListaComponent {
+  @Input()
+  productos: Producto[] = [];
+  @Output()
+  editar = new EventEmitter<Producto>();
+  @Output()
+  eliminar = new EventEmitter<number>();
+
+  editarProducto(producto: Producto) {
+    this.editar.emit(producto);
+  }
+  eliminarProducto(id: number) {
+    this.eliminar.emit(id);
+  }
 
 }
