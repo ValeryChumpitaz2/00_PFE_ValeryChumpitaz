72fd21d6b9d92f940ca1b8b7bae87e52970b8d20
ValeryChumpitaz2 <valerychumpitaz2@gmail.com>
2026-08-25 11:19:24 -0500

    S03-Estructura

---
+++ S03-sistema-productos/src/app/components/producto-formulario/producto-formulario.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-producto-formulario',
+  imports: [],
+  templateUrl: './producto-formulario.component.html',
+  styleUrl: './producto-formulario.component.css'
+})
+export class ProductoFormularioComponent {
+
+}
