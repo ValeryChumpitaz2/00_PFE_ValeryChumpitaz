694d8cfefd802c545cd75338ec8d84361fafcd07
ValeryChumpitaz2 <valerychumpitaz2@gmail.com>
2026-08-25 12:11:33 -0500

    Producto form ts

--- S03-sistema-productos/src/app/components/producto-formulario/producto-formulario.component.ts
+++ S03-sistema-productos/src/app/components/producto-formulario/producto-formulario.component.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Producto } from '../../app.component';
 
 @Component({
   selector: 'app-producto-formulario',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './producto-formulario.component.html',
   styleUrl: './producto-formulario.component.css'
 })
 export class ProductoFormularioComponent {
+@Input() producto: Producto | null = null;
+@Output() guardar = new EventEmitter<Producto>();
 
+productoForm: Producto = {
+  id: 0,
+  nombre: '',
+  precio: 0,
+  stock: 0
+};
+
+guardarProducto() {
+  this.guardar.emit({
+    ...this.productoForm
+  });
+  this.limpiar();
+}
+limpiar() {
+  this.productoForm = {
+    id: 0,
+    nombre: '',
+    precio: 0,
+    stock: 0
+  };
+}
 }
