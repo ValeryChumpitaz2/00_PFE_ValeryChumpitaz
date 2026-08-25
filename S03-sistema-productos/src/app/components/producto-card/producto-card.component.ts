e0d299d90a57d96735f787abac7d7d4f94d57de2
ValeryChumpitaz2 <valerychumpitaz2@gmail.com>
2026-08-25 12:07:10 -0500

    card

--- S03-sistema-productos/src/app/components/producto-card/producto-card.component.ts
+++ S03-sistema-productos/src/app/components/producto-card/producto-card.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Producto } from '../../app.component';
 
 @Component({
   selector: 'app-producto-card',
@@ -7,5 +8,16 @@ import { Component } from '@angular/core';
   styleUrl: './producto-card.component.css'
 })
 export class ProductoCardComponent {
-
+@Input()
+producto! : Producto;
+@Output()
+editar = new EventEmitter<Producto>();
+@Output()
+eliminar = new EventEmitter<number>();
+editarProducto() {
+    this.editar.emit(this.producto);
+  }
+  eliminarProducto() {
+    this.eliminar.emit(this.producto.id);
+  }
 }
