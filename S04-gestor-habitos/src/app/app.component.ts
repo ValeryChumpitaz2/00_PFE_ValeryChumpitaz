0cec32b87a299895a4b2f77eaa3c900bb5705668
ValeryChumpitaz2 <valerychumpitaz2@gmail.com>
2026-08-28 08:43:05 -0500

    app.component.ts

--- S04-gestor-habitos/src/app/app.component.ts
+++ S04-gestor-habitos/src/app/app.component.ts
@@ -1,12 +1,41 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Habito } from './models/habito';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HabitoFormularioComponent } from './components/habito-formulario/habito-formulario.component';
+import { HabitoListaComponent } from './components/habito-lista/habito-lista.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [NavbarComponent, HabitoFormularioComponent, HabitoListaComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'S04-gestor-habitos';
+habitos: Habito[]=[
+  {
+    id: 1,
+    nombre: 'Tomar agua',
+    categoria: 'Salud',
+    completado: true
+  },
+  {
+    id: 2,
+    nombre: 'Leer 20 minutos',
+    categoria: 'Estudio',
+    completado: false
+  },
+  {
+    id: 3,
+    nombre: 'Hacer ejercicio',
+    categoria: 'Salud',
+    completado: false
+  }
+];
+
+agregarHabito(habito: Habito){
+  habito.id = this.habitos.length + 1;
+  this.habitos.push(habito);
+}
+
+
 }
