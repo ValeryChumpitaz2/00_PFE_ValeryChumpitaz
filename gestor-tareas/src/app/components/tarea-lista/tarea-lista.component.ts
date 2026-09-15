55db172a418fbb992539f1f314c578123f7e50eb
ValeryChumpitaz2 <valerychumpitaz2@gmail.com>
2026-09-15 16:08:38 -0500

    tarea html

--- gestor-tareas/src/app/components/tarea-lista/tarea-lista.component.ts
+++ gestor-tareas/src/app/components/tarea-lista/tarea-lista.component.ts
@@ -1,15 +1,20 @@
 import { Component, inject } from '@angular/core';
 import { TareaService } from '../../services/tarea.service';
 
-
 @Component({
   selector: 'app-tarea-lista',
   imports: [],
   templateUrl: './tarea-lista.component.html',
   styleUrl: './tarea-lista.component.css'
-})
-export class TareaListaComponent {
-  private tareaService = inject(TareaService);
 
+
+})
+
+export class TareaListaComponent {
+
+  private tareaService = inject(TareaService);
   tareas = this.tareaService.obtenerTareas();
+
+
 }
+
