d4f80083201935799236bdb9b0bec0cc7d698da5
ValeryChumpitaz2 <valerychumpitaz2@gmail.com>
2026-09-18 08:52:00 -0500

    lista.ts

--- catalago-productos/src/app/components/producto-lista/producto-lista.component.ts
+++ catalago-productos/src/app/components/producto-lista/producto-lista.component.ts
@@ -1,11 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ProductoService } from '../../services/producto.service';
+
+
 
 @Component({
   selector: 'app-producto-lista',
+  standalone: true,
   imports: [],
   templateUrl: './producto-lista.component.html',
   styleUrl: './producto-lista.component.css'
 })
 export class ProductoListaComponent {
+  private productoService = inject(ProductoService);
+  productos: any[] = [];
 
+  nombre = '';
+  precio = 0;
+  imagen = '';
+
+  ngOnInit() {
+    this.cargarProductos();
+  }
+
+  //GET - OBTENER PRODUCTOS
+
+  cargarProductos() {
+    this.productoService.obtenerProductos().subscribe(respuesta => {
+      this.productos = respuesta.products;
+    });
+  }
+
+  //POST - REGISTRAR PRODUCTO
+
+  registrarProducto() {
+    if ( this.nombre || this.precio || this.imagen) {
+      alert('Completa todos los campos');
+      return;
+   }
+   const nuevoProducto = {
+    title: this.nombre,
+    price: this.precio,
+    thumbnail: this.imagen
+   };
+   this.productoService.registrarProducto(nuevoProducto).subscribe(respuesta => {
+    console.log('Producto registrado:', respuesta);
+    this.productos.unshift(respuesta);
+    this.nombre = '';
+    this.precio = 0;
+    this.imagen = '';
+   });
+   }
 }
