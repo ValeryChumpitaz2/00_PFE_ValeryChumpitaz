aada2f63ace7e4ebf38c92f26dfc2273694f58ad
ValeryChumpitaz2 <valerychumpitaz2@gmail.com>
2026-09-18 08:38:04 -0500

    servicio

--- catalago-productos/src/app/services/producto.service.ts
+++ catalago-productos/src/app/services/producto.service.ts
@@ -1,9 +1,23 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductoService {
 
-  constructor() { }
+  private http = inject(HttpClient);
+
+  private apiUrl = 'https://dummyjson.com/products';
+
+  //METODO GET
+  obtenerProductos() {
+    return this.http.get<any>(this.apiUrl);
+  }
+
+  //METODO POST
+  registrarProducto(producto: any) {
+    return this.http.post<any>(`${this.apiUrl}/add`, producto);
+  }
+
 }
