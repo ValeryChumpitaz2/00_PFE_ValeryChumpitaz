a352ff3992010094bc74a2e4801d5921ef597239
ValeryChumpitaz2 <valerychumpitaz2@gmail.com>
2026-09-18 08:29:24 -0500

    appconfig

---
+++ catalago-productos/src/app/components/producto-lista/producto-lista.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-producto-lista',
+  imports: [],
+  templateUrl: './producto-lista.component.html',
+  styleUrl: './producto-lista.component.css'
+})
+export class ProductoListaComponent {
+
+}
