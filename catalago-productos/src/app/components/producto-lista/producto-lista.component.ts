b5c9b15f02024027e2a76b073125004c22a64a48
ValeryChumpitaz2 <valerychumpitaz2@gmail.com>
2026-09-18 09:31:13 -0500

    completo

--- catalago-productos/src/app/components/producto-lista/producto-lista.component.ts
+++ catalago-productos/src/app/components/producto-lista/producto-lista.component.ts
@@ -1,6 +1,6 @@
 import { Component, inject } from '@angular/core';
 import { ProductoService } from '../../services/producto.service';
-import { FormsModule } from '@angular/forms'; 
+import { FormsModule } from '@angular/forms';
 
 
 @Component({
@@ -10,44 +10,109 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './producto-lista.component.html',
   styleUrl: './producto-lista.component.css'
 })
+
+
 export class ProductoListaComponent {
+
+
+
   private productoService = inject(ProductoService);
+
   productos: any[] = [];
 
+
+
   nombre = '';
+
+
+
+
   precio = 0;
+
+
   imagen = '';
 
+
+
+
+
+
   ngOnInit() {
+
+
     this.cargarProductos();
   }
 
-  //GET - OBTENER PRODUCTOS
 
   cargarProductos() {
-    this.productoService.obtenerProductos().subscribe(respuesta => {
-      this.productos = respuesta.products;
-    });
+
+    this.productoService.obtenerProductos()
+      .subscribe(respuesta => {
+        this.productos = respuesta.products;
+
+
+      });
+
+
   }
 
-  //POST - REGISTRAR PRODUCTO
-
   registrarProducto() {
-    if ( this.nombre || this.precio || this.imagen) {
+
+    if (!this.nombre || !this.precio || !this.imagen) {
+
+
+
       alert('Completa todos los campos');
+
+
       return;
-   }
-   const nuevoProducto = {
-    title: this.nombre,
-    price: this.precio,
-    thumbnail: this.imagen
-   };
-   this.productoService.registrarProducto(nuevoProducto).subscribe(respuesta => {
-    console.log('Producto registrado:', respuesta);
-    this.productos.unshift(respuesta);
-    this.nombre = '';
-    this.precio = 0;
-    this.imagen = '';
-   });
-   }
+    }
+
+
+
+
+    const nuevoProducto = {
+
+
+
+
+      title: this.nombre,
+
+
+      price: this.precio,
+
+
+
+
+      thumbnail: this.imagen
+
+
+    };
+
+    this.productoService.registrarProducto(nuevoProducto)
+      .subscribe(respuesta => {
+        console.log('Producto registrado:', respuesta);
+
+
+
+
+
+        this.productos.unshift(respuesta);
+
+        this.nombre = '';
+
+
+        this.precio = 0;
+
+
+        this.imagen = '';
+
+
+      });
+
+
+  }
+
+
 }
+
